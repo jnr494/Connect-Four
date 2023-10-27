b42cf402493c0749c42d00c501db6494e318aafb
Magnus Frandsen <mrgnamus@gmail.com>
2023-10-28 01:33:47 +0200

    PlayConnect4: Add methods create_player, prepare_game and start_game

--- PlayConnect4.py
+++ PlayConnect4.py
@@ -13,19 +13,83 @@ import numpy as np
 
 class PlayConnect4:
     
+    game = None
     human_color_wish: int = 1 #Red
     human_start_wish: int = 1 #Not Starting
     difficulty: str = ""
     _quick_start: bool = False
+    _debug: bool = False
     
-    def __init__(self):
-        pass
+    def __init__(self, game):
+        self.game = game
     
-    def start_game(self):
+    def setup_game(self):
+
         self.does_human_want_to_play()
+        
+        #Get Human game specifications
         if not self._quick_start:
             self.get_human_start_wish()
             self.get_human_color_wish()
+        self._get_human_difficulty_wish
+        
+        #Create opponent player        
+        self._create_player()
+    
+    def prepare_game(self):
+        self._player_turn = 0
+        if self.human_start_wish == self._player_turn:
+            self._player_values = [self.human_color_wish, self.human_color_wish*-1]
+        else:
+            self._player_values = [self.human_color_wish*-1, self.human_color_wish]
+            
+        self.game.reset()
+        
+    def start_game(self):
+        #init plot of game
+        self.game.plot_board_state(update = True)
+        
+        for i in range(6*7):
+            player_value = self._player_values[self._player_turn]
+            next_player_turn = (self._player_turn + 1) % 2
+            next_player_value = self._player_values[self._next_player_turn]
+            
+            #if human turn then ask human for action else ask AI player
+            if self._player_turn == self.human_start_wish:
+                available_actions = self.game.get_available_actions()
+                action = get_human_action(available_actions)
+            else:
+                clever_available_actions = self.game.get_clever_available_actions(player_value, next_player_value)
+                action = self.player.make_action(self.game, clever_available_actions)
+                print('I will play column',action+1)
+                if hasattr(self.player, 'winning_probability'):
+                    print('I estimate that my probability of winning is: ',str(round(self.player.winning_probability,4)*100)+'%')
+                    if self._debug:
+                        optimal_actions, q_values,amaf_q_values,nodes = self.player.get_optimal_actions_qvalues()
+                        print('Optimal actions:',[i+1 for i in optimal_actions])
+                        print('Q values:',[round(i,4) for i in q_values])
+                        print('AMAF Q values:',[round(i,4) for i in amaf_q_values])
+                        for idx, node in enumerate(nodes):
+                            print('Number of visits '+str(idx)+':',node['no_visits_actions'],'Q:',tuple(np.round(node['q_values'],3)),'AA:',node['actions']+1)
+            
+            #perform action and plot game
+            game_over = self.game.place_disc(action, player_value)
+            self.game.plot_board_state(update = True)
+            
+            #Check if game is over
+            if game_over:
+                if self._player_turn != self.human_start_wish:
+                    print("YOU SUCK! I knew it...")
+                else:
+                    print("God damn it... You are the master.")
+                input("Press Escape to stop the game: ")
+                sys.exit()
+            
+            self._player_turn = next_player_turn
+        
+        print("We are draw... Good game.")
+        input("Press Escape to stop the game: ")
+        sys.exit()
         
     def _does_human_want_to_play(self) -> None:
         answer = input('Hello human! Do you want to play a game? [yes,no]: ')
@@ -93,7 +157,49 @@ class PlayConnect4:
             difficulty = input("Answer not valid. Please choose a valid difficulty [easy,normal,hard,god]: ")    
         
         self.difficulty: str = difficulty.lower()
+    
+    #Create AI player based on difficulty
+    def _create_player(self):
+        time.sleep(0.25)
+        if self.difficulty in ['easy','e']:
+            self.player = Connect4Players.RandomPlayer()
+            print("Ok... pussy. Let's play...")
+        elif self.difficulty in ['normal','n']:
+            game = Connect4Game.Connect4()
+            self.player = Connect4Players.MCTSPlayer(game = game, 
+                                                player = self.human_color_wish*-1, 
+                                                next_player = self.human_color_wish, 
+                                                max_count = 5e2, 
+                                                max_depth = 2, 
+                                                confidence_value = 2**2,
+                                                rave_param=None)
+            print("Ok. Let's see what you can do.")
+        elif self.difficulty in ['hard','h']:
+            game = Connect4Game.Connect4()
+            self.player = Connect4Players.MCTSPlayer(game = game, 
+                                                player = self.human_color_wish*-1, 
+                                                next_player = self.human_color_wish, 
+                                                max_count = 2e3, 
+                                                max_depth = 5, 
+                                                confidence_value = 2**2,
+                                                rave_param=2**0) 
+            print("You are brave. Let's go!")
+        elif self.difficulty in ['god','g']:
+            game = Connect4Game.Connect4()
+            self.player = Connect4Players.MCTSPlayer(game = game, 
+                                                player = self.human_color_wish*-1, 
+                                                next_player = self.human_color_wish, 
+                                                max_count = 1e4,
+                                                max_depth = 100, 
+                                                confidence_value = 2**2,
+                                                rave_param=2**0)
+            print("You are a dead man... Let's go!")
+        else:
+            raise
         
+        
+    
+    
     
 def introduction():
     debug_mode = False
