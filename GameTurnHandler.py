51a7ffae4c837b5019cb71adb9694343e7a648c0
Magnus Frandsen <mrgnamus@gmail.com>
2023-10-29 17:30:50 +0100

    GameTurnHandler: add functionality to create a copy turnhandler at current state

--- GameTurnHandler.py
+++ GameTurnHandler.py
@@ -11,34 +11,54 @@ class GameTurnHandler:
     
     _current_player_value: int
     _next_player_value: int
+    _player_values: list[int]
     
     def __init__(self):
         pass
     
-    def reset(self, player_values: list[int]):
-        self._player_values_cycle = itertools.cycle(player_values)
-        self.current_player_value = next(self._player_values_cycle)
-        self.next_player_value = next(self._player_values_cycle)
+    def reset(self, player_values: list[int], starting_position: int = 0):
+        self._player_values = player_values
+        
+        self._player_turn_cycle = itertools.cycle(range(len(self._player_values)))
+        self._player_turn_cycle = itertools.islice(self._player_turn_cycle, starting_position, None)
+        
+        self._current_player_turn = next(self._player_turn_cycle)
+        self._next_player_turn = next(self._player_turn_cycle)
+        
+        self._current_player_value = self._player_values[self._current_player_turn]
+        self._next_player_value = self._player_values[self._next_player_turn]
         
     def get_current_player_value(self) -> int:
-        return self.current_player_value
+        return self._current_player_value
     
     def get_next_player_value(self) -> int:
-        return self.next_player_value
+        return self._next_player_value
     
     def next_turn(self) -> None:
-        self.current_player_value = self.next_player_value
-        self.next_player_value = next(self._player_values_cycle)
+        self._current_player_turn = self._next_player_turn
+        self._current_player_value = self._next_player_value
+        
+        self._next_player_turn = next(self._player_turn_cycle)
+        self._next_player_value = self._player_values[self._next_player_turn]
     
+    def copy(self):
+        new_game_handler = GameTurnHandler()
+        new_game_handler.reset(self._player_values, self._current_player_turn)
+        return new_game_handler
     
 if __name__ == '__main__':
-    mylist = [1,2,3]
-    myGameTurnHandler = GameTurnHandler(mylist)
-    print(myGameTurnHandler.get_current_player_value(), myGameTurnHandler.get_next_player_value())
-    myGameTurnHandler.next_turn()
-    print(myGameTurnHandler.get_current_player_value(), myGameTurnHandler.get_next_player_value())
-    myGameTurnHandler.next_turn()
-    print(myGameTurnHandler.get_current_player_value(), myGameTurnHandler.get_next_player_value())
-    myGameTurnHandler.next_turn()
-    print(myGameTurnHandler.get_current_player_value(), myGameTurnHandler.get_next_player_value())
-    myGameTurnHandler.next_turn()
+    game_turn_handler = GameTurnHandler()
+    game_turn_handler.reset([4,5,6,7,9],2)
+    print(game_turn_handler.get_current_player_value(),game_turn_handler.get_next_player_value())
+    game_turn_handler.next_turn()
+    print(game_turn_handler.get_current_player_value(),game_turn_handler.get_next_player_value())
+    game_turn_handler.next_turn()
+    print(game_turn_handler.get_current_player_value(),game_turn_handler.get_next_player_value())
+    
+    new_game_turn_handler = game_turn_handler.copy()
+    print("Copy")
+    print(new_game_turn_handler.get_current_player_value(),new_game_turn_handler.get_next_player_value())
+    new_game_turn_handler.next_turn()
+    print(new_game_turn_handler.get_current_player_value(),new_game_turn_handler.get_next_player_value())
+    game_turn_handler.next_turn()
+    
