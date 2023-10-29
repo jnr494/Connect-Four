5577c07ca242239c27b614b1400ccbc5bab0b3c1
Magnus Frandsen <mrgnamus@gmail.com>
2023-10-29 15:29:35 +0100

    logger: remove console handler

--- Logger.py
+++ Logger.py
@@ -21,20 +21,13 @@ class Logger:
         file_handler = logging.FileHandler('logs/PlayConnect4.log')
         file_handler.setLevel(logging.DEBUG)
         
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.INFO)
-        
         #Create formatters and add it to handlers
         file_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         file_handler.setFormatter(file_format)
         
-        console_format = logging.Formatter('%(message)s')
-        console_handler.setFormatter(console_format)
-        
         # Add handlers to the logger
         logger.addHandler(file_handler)
-        logger.addHandler(console_handler)
-        print(logger.handlers)
+        
         return logger
     
     
