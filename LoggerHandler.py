9aa09d0ae8769e472feedefdb579d931e4568311
Magnus Frandsen <mrgnamus@gmail.com>
2023-10-30 01:22:17 +0100

    format with ruff

--- LoggerHandler.py
+++ LoggerHandler.py
@@ -1,27 +1,30 @@
 import logging
 
+
 class LoggerHandler:
-    
     def get_playconnect4_logger(self) -> logging.Logger:
         logger = logging.getLogger("PlayConnect4")
         logger.setLevel(logging.DEBUG)
-        
+
         if logger.hasHandlers():
             return logger
-        
+
         # Create handlers
-        file_handler = logging.FileHandler('logs/PlayConnect4.log')
+        file_handler = logging.FileHandler("logs/PlayConnect4.log")
         file_handler.setLevel(logging.DEBUG)
-        
-        #Create formatters and add it to handlers
-        file_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        # Create formatters and add it to handlers
+        file_format = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
         file_handler.setFormatter(file_format)
-        
+
         # Add handlers to the logger
         logger.addHandler(file_handler)
-        
+
         return logger
-    
+
+
 if __name__ == "__main__":
     logger_handler = LoggerHandler()
     logger = logger_handler.get_playconnect4_logger()
