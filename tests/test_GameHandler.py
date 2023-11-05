2c41f9ca0a03eccd51f3234905b23405925dbeed
Magnus Frandsen <mrgnamus@gmail.com>
2023-11-06 00:48:28 +0100

    add test normal vs god

--- tests/test_GameHandler.py
+++ tests/test_GameHandler.py
@@ -25,8 +25,24 @@ class Connect4GameTests(unittest.TestCase):
         winners = game_handler.play_n_games(number_of_games)
 
         self.assertAlmostEqual(np.sum(winners),-1*number_of_games)
-    
+
     def test_normal_vs_hard_mcts(self: "Connect4GameTests") -> None:
+        config_handler = ConfigHandler.ConfigHandler()
+        logger_handler = LoggerHandler.LoggerHandler(config_handler)
+
+        game_turn_handler = GameTurnHandler.GameTurnHandler([1,-1])
+        game = Connect4Game.Connect4(game_turn_handler=game_turn_handler)
+
+        player0 = MCTSPlayerFactory.MCTSPlayerFactory.create_player(game, 1, -1, "normal", config_handler)
+        player1 = MCTSPlayerFactory.MCTSPlayerFactory.create_player(game, -1, 1, "god", config_handler)
+        game_handler = Connect4GameHandler.Connect4GameHandler(game, player0, player1,logger_handler,config_handler)
+        number_of_games = 10
+        winners = game_handler.play_n_games(number_of_games)
+
+        self.assertLess(np.sum(winners),0)
+
+
+    def test_normal_vs_god_mcts(self: "Connect4GameTests") -> None:
         config_handler = ConfigHandler.ConfigHandler()
         logger_handler = LoggerHandler.LoggerHandler(config_handler)
 
